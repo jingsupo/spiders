0a40ed397ae89892bdd3d7e604d923e62668831b
jingsupo <jingsupo@outlook.com>
2020-01-14 23:48:06 +0800

    更改爬虫标题提取策略

--- meitulu/meitulu/spiders/mm.py
+++ meitulu/meitulu/spiders/mm.py
@@ -6,27 +6,16 @@ from ..items import MeituluItem
 class MmSpider(scrapy.Spider):
     name = 'mm'
     allowed_domains = ['www.meitulu.com']
-    start_urls = ['https://www.meitulu.com/item/1025.html']  # [Bomb.TV] 逢泽莉娜 Rina Aizawa 2011-01
-    # start_urls = ['https://www.meitulu.com/item/4817.html']  # [YS Web] vol.671 安枝瞳 - 《濡れた瞳》 写真套图
-    # start_urls = ['https://www.meitulu.com/item/4092.html']  # [YS Web] Vol.635 菜乃花 Nanoka 写真集
-    # start_urls = ['https://www.meitulu.com/item/3985.html']  # [@misty] No.247 – 爱田樱(あいださくら Sakura Aida) 写真套图
-    # start_urls = ['https://www.meitulu.com/item/19185.html']  # [YS-Web] Vol.824 菜乃花 Nanoka
-    # start_urls = ['https://www.meitulu.com/item/13191.html']  # [Wanibooks] #162 橋本梨菜 Rina Hashimoto 写真套图
-    title = ''
+    start_urls = ['https://www.meitulu.com/item/16362.html']
 
     def parse(self, response):
-        page_no = response.xpath('//div[@id="pages"]/span/text()').extract_first()
         img_list = response.xpath('//img[@class="content_img"]/@src').extract()
         for img in img_list:
             item = MeituluItem()
             item['img_src'] = img
             item['img_name'] = img.split('/')[-1]
-            if page_no == '1':
-                item['title'] = response.xpath('//h1/text()').extract_first()
-                self.title = item['title']
-            else:
-                item['title'] = self.title
+            item['title'] = response.xpath('//title/text()').extract_first().split('_')[0]
             yield item
-        page = 'https://www.meitulu.com' + response.xpath('//div[@id="pages"]/a[text()="下一页"]/@href').extract_first()
-        if page:
-            yield scrapy.Request(url=page, callback=self.parse)
+        next_page = response.xpath('//div[@id="pages"]/a[text()="下一页"]/@href').extract_first()
+        if next_page:
+            yield scrapy.Request(url='https://www.meitulu.com'+next_page, callback=self.parse)
